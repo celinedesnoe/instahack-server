2cd4d69350a449371cb89ab8d1895fa523330490
Anoopdeep Bal <anoopdeep@gmail.com>
2019-02-27 17:59:04 +0100

    ab: server-side app.js wired

--- app.js
+++ app.js
@@ -1,58 +1,66 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
 
+// run code inside of passport-setup.js
+require("./config/passport-setup.js");
 
 mongoose
-  .connect('mongodb://localhost/instahack-server', {useNewUrlParser: true})
+  .connect("mongodb://localhost/instahack-server", { useNewUrlParser: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// Express View engine setup
+// allow cross-origin resource-sharing -- CORS
+// (allows access to the API from other domains/origins)
+app.use(
+  cors({
+    // receive cookies from other domains/origins
+    credentials: true,
+    // only these domains/origins can access the API
+    origin: ["http://localhost:3000", "www.blah.com"]
+  })
+);
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    // session secret must be different for every app
+    secret: process.env.SESSION_SECRET,
+    // save session info inside MongoDB
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// activate some of the passport methods in our routes
+app.use(passport.initialize());
+// load the logged-in user's information once we are logged-in
+app.use(passport.session());
 
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-const index = require('./routes/index');
-app.use('/', index);
-
+// all routes in the phone router will start with "/api"
 
 module.exports = app;
