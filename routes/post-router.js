be753f767ba1548bd46ac6d48f7123972c3755e3
Anoopdeep Bal <anoopdeep@gmail.com>
2019-03-05 11:04:54 +0100

    ab: Likes and Comments ready to be tested by someone else

--- routes/post-router.js
+++ routes/post-router.js
@@ -6,49 +6,23 @@ const Comment = require("../models/comment-model.js");
 const router = express.Router();
 
 // ##################################################################################
-// GET DETAILS OF THE POST
+// GET DETAILS OF THE POST (LIKES, COMMENTS, PICTURE, USER, DATE POSTED)
 // WITH THE USER OBJECT THROUGH POPULATE ("username_id") in the field "username_id"
 // ##################################################################################
 
-// router.get("/p/:postId", (req, res, next) => {
-//   const { postId } = req.params;
-
-//   Post.findById(postId)
-//     .populate("username_id")
-//     .then(postDoc => {
-//       Comment.find({ post_id: { $eq: postId } })
-//         .then(commentDoc => {
-
-//           const commentsArray = [];
-
-//           commentDoc.map(oneComment => {
-//             const commentObject = User.findById(oneComment.username_id)
-//               .then(userDoc => {
-//                 return {
-//                   pic: userDoc.profilePic,
-//                   username: userDoc.username,
-//                   content: oneComment.content
-//                 };
-//               })
-//               .catch(err => next(err));
-
-//             commentsArray.push(commentObject);
-//           });
-
-//           res.json({ post: postDoc, comments: commentsArray });
-//         })
-//         .catch(err => next(err));
-//     })
-//     .catch(err => next(err));
-// });
-
 router.get("/p/:postId", (req, res, next) => {
   const { postId } = req.params;
-
+  // return res.send(currentUser);
   Post.findById(postId)
     .populate("username_id")
     .then(postDoc => {
+      var likeState = false;
+      if (postDoc.likedBy.indexOf() > -1) {
+        likeState = true;
+      }
+
       Comment.find({ post_id: { $eq: postId } })
+        .sort({ createdAt: -1 })
         .populate("username_id")
         .then(commentResults => {
           res.json({ post: postDoc, comments: commentResults });
@@ -58,12 +32,48 @@ router.get("/p/:postId", (req, res, next) => {
     .catch(err => next(err));
 });
 
+// ##################################################################################
+// ADD COMMENT (NEW COLLECTION -- TAKES COMMENTER, POST, AND COMMENT CONTENT)
+// ##################################################################################
+
 router.post("/process-comment", (req, res, next) => {
   const { username_id, post_id, content } = req.body;
 
   Comment.create({ username_id, post_id, content })
     .then(commentDoc => res.json(commentDoc))
     .next(err => next(err));
+});
+
+// ##################################################################################
+// ADD A LIKE TO THE POST'S LIKEDBY ARRAY
+// ##################################################################################
+
+router.post("/process-like", (req, res, next) => {
+  const { post, liker } = req.body;
+
+  // return res.send(post);
+  // return res.send(liker);
+
+  Post.findByIdAndUpdate(post, { $push: { likedBy: liker } })
+    .then(postDoc => res.json(postDoc))
+    .catch(err => next(err));
+});
+
+// ##################################################################################
+// REMOVE A LIKE FROM A POST'S LIKEDBY ARRAY
+// ##################################################################################
+
+router.post("/process-unlike", (req, res, next) => {
+  const { post, unliker } = req.body;
+
+  // return res.send(post);
+  // return res.send(liker);
+
+  Post.findByIdAndUpdate(post, { $pull: { likedBy: unliker } })
+    .then(postDoc => {
+      res.json(postDoc);
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
