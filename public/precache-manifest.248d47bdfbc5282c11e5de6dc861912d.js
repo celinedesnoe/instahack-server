68832dba11bd318e1c45eca0c7f9070bcfcb71db
celinedesnoe <celine.desnoe@skema.edu>
2019-03-06 18:27:29 +0100

    Add React production files

---
+++ public/precache-manifest.248d47bdfbc5282c11e5de6dc861912d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ac847b0dd3ae775d4b8d4b8192a43f",
+    "url": "/static/media/chatline.d5ac847b.png"
+  },
+  {
+    "revision": "9b4ebc742d9ba83d7d0c",
+    "url": "/static/css/main.08edf115.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "99b48e60399c0221886f78fbc0fcb75c",
+    "url": "/static/media/profilefull.99b48e60.png"
+  },
+  {
+    "revision": "dbb394ba336372b5096f",
+    "url": "/static/js/2.dbb394ba.chunk.js"
+  },
+  {
+    "revision": "c39936047e511918c243079f85f979a3",
+    "url": "/static/media/Instagram_logo.c3993604.png"
+  },
+  {
+    "revision": "dea8166a0503f30e6d9a3333c928b138",
+    "url": "/static/media/likeempty.dea8166a.png"
+  },
+  {
+    "revision": "a07e6920e160ffd04698ccfd54833d83",
+    "url": "/static/media/plus.a07e6920.png"
+  },
+  {
+    "revision": "3a4b4bab2d2f03753988181c17fa1d6f",
+    "url": "/static/media/optionpicture.3a4b4bab.png"
+  },
+  {
+    "revision": "2aa219e101fdde2239777899f7e79cd5",
+    "url": "/static/media/plusprofile.2aa219e1.png"
+  },
+  {
+    "revision": "960c4a5f8944493aaefd1e49db4ad358",
+    "url": "/static/media/iglogo.960c4a5f.png"
+  },
+  {
+    "revision": "9b4ebc742d9ba83d7d0c",
+    "url": "/static/js/main.9b4ebc74.chunk.js"
+  },
+  {
+    "revision": "d8c7801dc5059a06fd296c489df08500",
+    "url": "/static/media/likefullred.d8c7801d.png"
+  },
+  {
+    "revision": "aee10115dcceba291e33f685097c6f4a",
+    "url": "/static/media/share.aee10115.png"
+  },
+  {
+    "revision": "160ea7679b885a8b9829db0d016a5f0e",
+    "url": "/static/media/pick.160ea767.png"
+  },
+  {
+    "revision": "038790594e78a4b3c32ef28644b0a54a",
+    "url": "/static/media/homeempty.03879059.png"
+  },
+  {
+    "revision": "91feeb40a26142d066cbe5b8c6a4eb36",
+    "url": "/static/media/homefull.91feeb40.png"
+  },
+  {
+    "revision": "676ca0f62eebae934b39d70fb93dfd02",
+    "url": "/static/media/searchempty.676ca0f6.png"
+  },
+  {
+    "revision": "5ba9f576d0c1f7c1519ed7d55c29b7b7",
+    "url": "/static/media/searchfull.5ba9f576.png"
+  },
+  {
+    "revision": "270d191ce4756341b519e35ab159f76a",
+    "url": "/static/media/likefull.270d191c.png"
+  },
+  {
+    "revision": "6d8994c61cd4782d459d59e8fd04ad43",
+    "url": "/static/media/profileempty.6d8994c6.png"
+  },
+  {
+    "revision": "dbb394ba336372b5096f",
+    "url": "/static/css/2.887a6c41.chunk.css"
+  },
+  {
+    "revision": "16629f9af25deac0710a1ac2d4524b3f",
+    "url": "/index.html"
+  }
+];
