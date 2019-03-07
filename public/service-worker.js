536222cfc33079c7d010cf74221ffd04dc65aa5a
celinedesnoe <celine.desnoe@skema.edu>
2019-03-07 13:20:57 +0100

    ok

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,13 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.e57af9d9d3c715b697a5ddf76a78b19b.js"
-=======
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
->>>>>>> c4f8f61b33ee2786594a2e414a953d146fd7b395
-);
+importScripts("/precache-manifest.e57af9d9d3c715b697a5ddf76a78b19b.js");
 
 importScripts("/precache-manifest.248d47bdfbc5282c11e5de6dc861912d.js");
 
