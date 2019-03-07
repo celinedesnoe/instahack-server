5a33db19a21c87e5228fbf0d6593fd984b521ae9
Anoopdeep Bal <anoopdeep@gmail.com>
2019-03-07 20:24:47 +0100

    ab: changed comment sort to 1

--- routes/post-router.js
+++ routes/post-router.js
@@ -18,7 +18,7 @@ router.get("/p/:postId", (req, res, next) => {
     .populate("likedBy", "profilePic followers")
     .then(postDoc => {
       Comment.find({ post_id: { $eq: postId } })
-        .sort({ createdAt: 1 })
+        .sort({ createdAt: -1 })
         .populate("username_id", "username")
         .then(commentResults => {
           res.json({ post: postDoc, comments: commentResults });
